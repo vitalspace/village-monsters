d370bfe8c8741c0def8b8bce5eb315ab04c6be10
vitalspace <myglobalsounds@gmail.com>
2024-10-23 00:29:50 -0600

    updated

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
   import { cameraIndex, cameraPositions } from "../../stores/stores";
-  
+
   function nextPosition() {
-    cameraIndex.update(index => (index + 1) % cameraPositions.length);
+    cameraIndex.update((index) => (index + 1) % cameraPositions.length);
   }
 
   function prevPosition() {
-    cameraIndex.update(index => (index - 1 + cameraPositions.length) % cameraPositions.length);
+    cameraIndex.update(
+      (index) => (index - 1 + cameraPositions.length) % cameraPositions.length
+    );
   }
 </script>
+
+<div class="fixed top-10 right-0 mx-auto w-1/5 rounded-2xl py-4 text-center">
+  <a href="https://github.com/vitalspace/village-monsters" target="_blank">
+    <img class="w-8 h-8" src="./github.svg" alt="" />
+  </a>
+</div>
 
 <div
   class="fixed bottom-10 left-0 right-0 mx-auto w-1/5 rounded-2xl py-4 text-center bg-[rgba(60,52,52,0.7)] backdrop-blur-[4px] backdrop-saturate-[200%]"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,15 @@
   import Views from "$lib/components/Menu.svelte";
 </script>
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  ...
+<title>Village Monster | by Vitalspace</title>
+
+</head>
+
 <div>
   <Views />
   <App />
