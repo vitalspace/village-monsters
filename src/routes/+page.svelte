b2417d133bf3334c8524feaa1043ac32f67183b0
vitalspace <myglobalsounds@gmail.com>
2024-10-26 14:18:05 -0600

    updated title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,11 @@
 </script>
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   ...
-<title>Village Monster | by Vitalspace</title>
-
+  <title>Monster Village | by Vitalspace</title>
 </head>
 
 <div>
